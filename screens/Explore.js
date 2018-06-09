b2ad9b9d3c475638975d3e13bc6284395f495f12
ovarunendra <vpsingh016@gmail.com>
2018-06-09 10:40:12 +0530

    Added horizontal scroll views

--- screens/Explore.js
+++ screens/Explore.js
@@ -7,8 +7,15 @@ import {
     Platform,
     StatusBar,
     TextInput,
+    Dimensions,
+    ScrollView,
+    Image,
 } from "react-native";
 import Icon from 'react-native-vector-icons/Ionicons';
+
+import Category from './components/Explore/Category';
+
+const { height, width } = Dimensions.get('window');
 
 class Explore extends Component {
 
@@ -42,17 +49,49 @@ class Explore extends Component {
                             />
                         </View>
                     </View>
+                    <ScrollView scrollEventThrottle={16}>
+                    <View style={{ flex: 1, backgroundColor: 'white', paddingTop: 20 }}>
+                            <Text style={{ fontSize: 24, fontWeight: '700', paddingHorizontal: 20 }}>
+                                What can we help you find, Varun?
+                            </Text>
+
+                            <View style={{ height: 130, marginTop: 20 }}>
+                                <ScrollView
+                                    horizontal={true}
+                                    showsHorizontalScrollIndicator={false}
+                                >
+                                    <Category imageUri={require('../assets/home.jpg')}
+                                        name="Home"
+                                    />
+                                    <Category imageUri={require('../assets/experiences.jpg')}
+                                        name="Experiences"
+                                    />
+                                    <Category imageUri={require('../assets/restaurant.jpg')}
+                                        name="Resturant"
+                                    />
+                                </ScrollView>
+                            </View>
+                            <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
+                                <Text style={{ fontSize: 24, fontWeight: '700' }}>
+                                    Introducing Stay Finder
+                                </Text>
+                                <Text style={{ fontWeight: '100', marginTop: 10 }}>
+                                    A new selection of homes verified for quality & comfort
+
+                                </Text>
+                                <View style={{ width: width - 40, height: 200, marginTop: 20 }}>
+                                    <Image
+                                        style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 5, borderWidth: 1, borderColor: '#dddddd' }}
+                                        source={require('../assets/home.jpg')}
+                                    />
+
+                                </View>
+                            </View>
+                        </View>
+                    </ScrollView>
                 </View>
             </SafeAreaView>
         );
     }
 }
 export default Explore;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-});
